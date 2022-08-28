0d4a589bf1217f950024c01abc9aacb05a70bf53
lvaolin <lvaolin@foresee.com.cn>
2022-08-28 21:58:07 +0800

    duck config

--- rpc-server/src/main/java/com/dhy/server/impl/UserServiceImpl.java
+++ rpc-server/src/main/java/com/dhy/server/impl/UserServiceImpl.java
@@ -1,10 +1,9 @@
 package com.dhy.server.impl;
 
-import com.dhy.duck.anntation.MyService;
 import com.dhy.server.itf.IUserServive;
 import com.dhy.server.dto.User;
 
-@MyService
+
 public class UserServiceImpl implements IUserServive {
     @Override
     public User getUserById(Long userId) {
